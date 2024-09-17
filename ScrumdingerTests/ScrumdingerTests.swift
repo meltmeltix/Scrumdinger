02c8ba8b22d74f61409d8e7d818792a001101353
Alessio Cameroni <alessioc.developer@gmail.com>
2024-09-17 10:53:37 +0200

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Alessio on 17/09/24.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
